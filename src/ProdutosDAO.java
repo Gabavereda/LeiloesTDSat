b304ceaf26ea09a78e523a38598fa77e2022a5e1
Glauber Vereda <gabavereda@gmail.com>
2024-08-17 15:27:14 -0300

    Ajustndo a funcionalidade de Salvar no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,18 +1,40 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ProdutosDAO {
 
-    Connection conn;
+    private Connection conn;
+    private conectaDAO conectdadao;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
 
+    public ProdutosDAO() {
+        this.conectdadao = new conectaDAO();
+        this.conn = conectdadao.connectDB();
+
+    }
+
     public void cadastrarProduto(ProdutosDTO produto) {
+
+        String sql = "INSERT INTO produtos (nome,valor) VALUES(?,?)";
+
+        try {
+            PreparedStatement prep = this.conn.prepareStatement(sql);
+
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+//            prep.setString(3, produto.getStatus());
+
+            prep.execute();
+
+        } catch (SQLException sqle) {
+            System.out.println("Não foi possivel cadastrar no banco " + sqle.getMessage());
+        }
 
         //conn = new conectaDAO().connectDB();
     }
